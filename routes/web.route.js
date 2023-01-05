0d0849e7781435c511ecd0d621e756e911444058
Wilson <wcastropaixao@gmail.com>
2023-01-05 15:19:17 -0300

    Implementa o CRUD de Educação
    //Javascript Node.js Express MongoDB APIs

--- routes/web.route.js
+++ routes/web.route.js
@@ -3,6 +3,7 @@ import { uploadFile, uploadImage } from '../utils/uploads.js';
 import auth from "../middlewares/auth.middleware.js"
 import AuthController from "../controllers/auth.controller.js";
 import ProfileController from "../controllers/profile.controller.js";
+import EducationController from "../controllers/education.controller.js";
 import InterestController from "../controllers/interest.controller.js";
 
 const router = express.Router();
@@ -12,5 +13,11 @@ router.post("/register", AuthController.register);
 
 router.get("/profiles/:id", auth, ProfileController.getProfileById);
 router.patch("/profiles/:id", uploadFile('profile', 'name'), auth, ProfileController.updateProfile);
+
+router.get("/educations", auth, EducationController.getAllEducation);
+router.get("/educations/:id", auth, EducationController.getEducationById);
+router.post('/educations', auth, EducationController.createEducation);
+router.patch('/educations/:id', auth, EducationController.updateEducation);
+router.delete('/educations/:id', auth, EducationController.deleteEducation);
 
 export default router;
